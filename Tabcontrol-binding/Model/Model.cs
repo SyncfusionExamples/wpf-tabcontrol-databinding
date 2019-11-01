3a8991f90700704ce60ad52d5834ea2f11807967
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2019-11-01 17:23:31 +0530

    Added DataBinding sample

---
+++ Tabcontrol-binding/Model/Model.cs
@@ -0,0 +1,62 @@
+﻿using Syncfusion.Windows.Shared;
+using Syncfusion.Windows.Tools.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabcontrol_binding
+{
+    public class Model : NotificationObject
+    {
+        private string header;
+        public string Header
+        {
+            get { return header; }
+            set
+            {
+                header = value;
+                this.RaisePropertyChanged("Header");
+            }
+        }
+
+        private string content;
+        public string Content
+        {
+            get { return content; }
+            set
+            {
+                content = value;
+                this.RaisePropertyChanged("Content");
+            }
+        }
+
+        private string image;
+        public string Image
+        {
+            get { return image; }
+            set
+            {
+                image = value;
+                this.RaisePropertyChanged("Image");
+            }
+        }
+
+        private ImageAlignment imageAlignment = ImageAlignment.LeftOfText;
+        public ImageAlignment ImageAlignment
+        {
+            get { return imageAlignment; }
+            set
+            {
+                imageAlignment = value;
+                this.RaisePropertyChanged("ImageAlignment");
+            }
+        }
+
+        public Model()
+        {
+
+        }
+    }
+}
---
+++ Tabcontrol-binding/ViewModel/ViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Syncfusion.Windows.Shared;
+using Syncfusion.Windows.Tools.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabcontrol_binding
+{
+    public class ViewModel : NotificationObject
+    {
+        private ObservableCollection<Model> tabItems;
+        public ObservableCollection<Model> TabItems
+        {
+            get { return tabItems; }
+            set
+            {
+                tabItems = value;
+                this.RaisePropertyChanged("TabItems");
+            }
+        }
+
+        private bool enableLabelEdit = true;
+        public bool EnableLabelEdit
+        {
+            get { return enableLabelEdit; }
+            set
+            {
+                enableLabelEdit = value;
+                this.RaisePropertyChanged("EnableLabelEdit");
+            }
+        }
+
+        private CloseButtonType closeButtonType=CloseButtonType.Both;
+        public CloseButtonType CloseButtonType
+        {
+            get { return closeButtonType; }
+            set
+            {
+                closeButtonType = value;
+                this.RaisePropertyChanged("CloseButtonType");
+            }
+        }
+
+        public ViewModel()
+        {
+            tabItems = new ObservableCollection<Model>();
+            PopulateCollection();
+        }
+
+        public void PopulateCollection()
+        {
+            Model model1 = new Model() { Header="tab1", Content= "This is the content of first tabitem.", Image="Images/sync.png" };
+            Model model2 = new Model() { Header="tab2", Content= "This is the content of second tabitem.", Image="Images/sync.png" };
+
+            tabItems.Add(model1);
+            tabItems.Add(model2);
+        }
+    }
+}
