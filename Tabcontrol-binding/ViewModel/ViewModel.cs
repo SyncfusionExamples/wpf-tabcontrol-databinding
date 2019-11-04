82677c2e4cd4767539bee8f6402dab7807bd8943
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2019-11-04 10:11:14 +0530

    Modified data binding sample

--- Tabcontrol-binding/ViewModel/ViewModel.cs
+++ Tabcontrol-binding/ViewModel/ViewModel.cs
@@ -20,30 +20,8 @@ namespace Tabcontrol_binding
                 tabItems = value;
                 this.RaisePropertyChanged("TabItems");
             }
-        }
-
-        private bool enableLabelEdit = true;
-        public bool EnableLabelEdit
-        {
-            get { return enableLabelEdit; }
-            set
-            {
-                enableLabelEdit = value;
-                this.RaisePropertyChanged("EnableLabelEdit");
-            }
-        }
-
-        private CloseButtonType closeButtonType=CloseButtonType.Both;
-        public CloseButtonType CloseButtonType
-        {
-            get { return closeButtonType; }
-            set
-            {
-                closeButtonType = value;
-                this.RaisePropertyChanged("CloseButtonType");
-            }
-        }
-
+        }        
+        
         public ViewModel()
         {
             tabItems = new ObservableCollection<Model>();
@@ -53,7 +31,7 @@ namespace Tabcontrol_binding
         public void PopulateCollection()
         {
             Model model1 = new Model() { Header="tab1", Content= "This is the content of first tabitem.", Image="Images/sync.png" };
-            Model model2 = new Model() { Header="tab2", Content= "This is the content of second tabitem.", Image="Images/sync.png" };
+            Model model2 = new Model() { Header="tab2", Content= "This is the content of second tabitem.", Image="Images/tabimage.png" };
 
             tabItems.Add(model1);
             tabItems.Add(model2);
